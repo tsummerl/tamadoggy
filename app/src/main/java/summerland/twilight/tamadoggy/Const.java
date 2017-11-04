732d3a57fdf8ca32a976f5b192423b7ec1366490
T <twisummerland@gmail.com>
2017-11-04 13:01:21 -0700

    Added basic logic for getting shared preference on main menu screen

---
+++ app/src/main/java/summerland/twilight/tamadoggy/Const.java
@@ -0,0 +1,14 @@
+package summerland.twilight.tamadoggy;
+
+/**
+ * Created by Jake on 11/1/2017.
+ */
+
+public final class Const {
+    public static final String SHARED_BASIC_GAME_DATA = "BASIC_GAME_DATA";
+    public static final String SHARED_NULL = "NULL_VALUE";
+    public static final String SHARED_DATE_LAST_GAME = "LAST_GAME";
+    private Const(){
+        throw new AssertionError();
+    }
+}
--- app/src/main/java/summerland/twilight/tamadoggy/MainMenuActivity.java
+++ app/src/main/java/summerland/twilight/tamadoggy/MainMenuActivity.java
@@ -1,13 +1,52 @@
 package summerland.twilight.tamadoggy;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainMenuActivity extends AppCompatActivity {
+import java.util.Date;
+
+public class MainMenuActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private SharedPreferences sPref;
+
+    Button buttonResume;
+    Button buttonNew;
+    Button buttonSettings;
+    Button buttonCredits;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+
+        buttonResume = findViewById(R.id.buttonResume);
+        buttonNew = findViewById(R.id.buttonNewGame);
+        buttonSettings = findViewById(R.id.buttonSettings);
+        buttonCredits = findViewById(R.id.buttonCredits);
+
+        sPref = getSharedPreferences(Const.SHARED_BASIC_GAME_DATA, Context.MODE_PRIVATE);
+        Date lastGame = new Date(sPref.getLong(Const.SHARED_DATE_LAST_GAME, 0));
+        if (lastGame.getTime() == 0L){
+            buttonResume.setEnabled(false);
+            buttonResume.setAlpha(0.5f);
+        }
+        else{
+            buttonResume.setEnabled(true);
+            buttonResume.setAlpha(1);
+        }
+        buttonResume.setOnClickListener(this);
+        buttonNew.setOnClickListener(this);
+        buttonCredits.setOnClickListener(this);
+        buttonSettings.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+
     }
 }
